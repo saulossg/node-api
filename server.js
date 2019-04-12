ce13f92dea96b9751bf7fbb78fece610d1282c48
Saulo <saulossg@gmail.com>
2019-04-11 23:15:10 -0300

    Configurando require-dir

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true
 });
+
+requireDir("./src/models");
 
 app.get("/", (req, res) => {
   res.send("Hello Rocketseat!!!!!!");
