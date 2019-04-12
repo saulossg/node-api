c95f9d338deaecf171b1a1933f6ea740ce2323a2
Saulo <saulossg@gmail.com>
2019-04-12 00:10:09 -0300

    Configurando o mongoose 2

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const requireDir = require("require-dir");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/nodeapi", {
+mongoose.connect("mongodb://127.0.0.1:27017/nodeapi", {
   useNewUrlParser: true
 });
 
 requireDir("./src/models");
 
+const Product = mongoose.model("Product");
+
 app.get("/", (req, res) => {
+  Product.create({
+    title: "React Native",
+    description: "Build native apps with REact",
+    url: "http://github.com/facebook/react-native"
+  });
+
   res.send("Hello Rocketseat!!!!!!");
 });
 
