cf000f8e211bd35f9eb476bc894a5bb7d0123a3f
Saulo <saulossg@gmail.com>
2019-04-15 21:35:34 -0300

    Criado rodas para update do registro.

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 const Product = mongoose.model("Product");
 
 module.exports = {
@@ -10,6 +9,20 @@ module.exports = {
 
   async store(req, res) {
     const product = await Product.create(req.body);
+
+    return res.json(product);
+  },
+
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+
+    return res.json(product);
+  },
+
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
 
     return res.json(product);
   }
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const routes = express.Router();
 const ProductController = require("./controllers/ProductController");
 
 routes.get("/products", ProductController.index);
+routes.get("/products/:id", ProductController.show);
 routes.post("/products", ProductController.store);
+routes.put("/products/:id", ProductController.update);
 
 module.exports = routes;
